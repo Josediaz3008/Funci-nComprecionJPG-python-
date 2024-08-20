f049a30a6f92571da16c8b0c7f058661678f3398
Jose A Diaz <118406561+Josediaz3008@users.noreply.github.com>
2024-08-20 10:54:42 -0600

    Create __manifest__.py
    se asegura que el archivo tenga todos los campos

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Image Compression',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Module to compress and upload images',
+    'depends': ['base', 'web'],
+    'data': [
+        'views/image_view.xml',
+    ],
+    'application': True,
+}
